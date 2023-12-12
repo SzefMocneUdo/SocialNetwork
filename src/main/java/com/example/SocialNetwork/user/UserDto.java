c6c93aabc65f5b911f9440bb3bd4ffbf2e840492
SzefMocneUdo <kacper160601@wp.pl>
2023-12-12 16:16:46 +0100

    JWT authentication and authorization

---
+++ src/main/java/com/example/SocialNetwork/user/UserDto.java
@@ -0,0 +1,26 @@
+package com.example.SocialNetwork.user;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
+
+public class UserDto {
+    @NotNull
+    @NotEmpty
+    private String firstName;
+
+    @NotNull
+    @NotEmpty
+    private String lastName;
+
+    @NotNull
+    @NotEmpty
+    private String password;
+    private String matchingPassword;
+
+    @NotNull
+    @NotEmpty
+    private String email;
+
+    // standard getters and setters
+}
